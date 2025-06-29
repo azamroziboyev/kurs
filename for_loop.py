20947e2fa876d2c7313d6a32bede44a84772aded
Azamjon Roziboyev <aroziboyev07@gmail.com>
2025-06-29 19:34:15 +0500

    for loop bo'yichamisollar for11 gacha

---
+++ for_loop.py
@@ -0,0 +1,55 @@
+"""For loop sikl mavusiga doir masalalar yechimlari"""
+#For1
+# k = input("k = ")
+# n = int(input("n = "))
+# print(k*n)
+
+#For2
+# a = int(input("a = "))
+# b = int(input("b = "))
+# count = 0
+# for num in range(a, b+1):
+#     print(num)
+#     count += 1
+# print(f"jami ekranga chiqarilgan raqamlar soni {count}")
+
+#For4
+# konfet_price = 50000
+# for i in range(1, 11):
+#     print(f"{konfet_price*i:,}".replace(",", " "))
+
+#For7
+# a = int(input("a = "))
+# b = int(input("b = "))
+# count = 0
+# for num in range(a, b+1):
+#     count += num
+# print(f"{a} dan {b} gacha bo'lgan butun solar yig'indisi {count} ga teng")
+
+#For9
+# a = int(input("a = "))
+# b = int(input("b = "))
+# sum_of_squares = 0
+# for i in range(a, b+1):
+#     sum_of_squares += i**2
+# print(sum_of_squares)
+
+#For10
+# n = int(input("n = "))
+# s = 0 
+# for i in range(1, n+1):
+#     s += (1 / i)
+# print(round(s,2))
+
+#For11
+# n = int(input("Enter n: "))  
+# total_sum = 0                
+# for k in range(n, 2*n + 1): 
+#     total_sum += k ** 2       
+# print(total_sum)              
+
+#For
+a = int(input("a = "))
+
+
+
