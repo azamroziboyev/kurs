42259bab0fcb9425ae4a72982e207766f770f36d
Azamjon Roziboyev <aroziboyev07@gmail.com>
2025-06-30 10:10:33 +0500

    .

---
+++ boolean_masalalar.py
@@ -0,0 +1,15 @@
+#boolean2
+# a = int(input("a = "))
+# print(a%2!=0)
+
+#boolean4
+# a = int(input("a = "))
+# b = int(input("b = "))
+# print(a>2 and b<=3)
+
+#boolean6
+a = int(input("a = "))
+b = int(input("b = "))
+c = int(input("c = "))
+
+print(a<=b<=c)
