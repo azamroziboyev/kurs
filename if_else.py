761a47323296ffa843712d7cfc2a16959b410423
Azamjon Roziboyev <aroziboyev07@gmail.com>
2025-07-03 03:13:05 +0500

    .

---
+++ if_else.py
@@ -0,0 +1,25 @@
+
+#if6
+# a = int(input(" a = "))
+# b = int(input(" b = "))
+# if a>b:
+#     print("katta son", a)
+# elif b>a:
+#     print("katta son", b)
+# else:
+#     print("ular ikkalasi teng")
+
+#if7
+"""savol tushunarsiz"""
+
+#if9
+"bu masala qoldi"
+
+#if10
+# a = int(input(" a = "))
+# b = int(input(" b = "))
+# if a!=b:
+#     a=b=a+b
+# else:
+#     a=b=0
+# print(a,b)
