8ce2dbe8c20a6657da74cf4ac5e2571eb4f9a38a
Azamjon Roziboyev <aroziboyev07@gmail.com>
2025-07-04 10:46:59 +0500

    .

--- if_else.py
+++ if_else.py
@@ -1,3 +1,24 @@
+# #if4
+# a = int(input("a = "))
+# b = int(input("b = "))
+# c = int(input("c = "))
+
+# min_number = a
+
+# if b < min_number and c > b:
+#     min_number = b
+# elif c <min_number and b > c:
+#     min_number = c
+
+# max_number = a
+
+# if b > max_number and c < b:
+#     max_number = b
+# elif c > max_number and b < c:
+#     max_number = c 
+
+# count = max_number - min_number - 2 
+# print(count)
 
 #if6
 # a = int(input(" a = "))
@@ -10,10 +31,19 @@
 #     print("ular ikkalasi teng")
 
 #if7
-"""savol tushunarsiz"""
+# a = int(input(" a = "))
+# b = int(input(" b = "))
+# if a<b:
+#     print("birinchi")
+# else:
+#     print("ikkinchi")
 
 #if9
-"bu masala qoldi"
+# a = int(input(" a = "))
+# b = int(input(" b = "))
+# if not a<b:
+#     a,b = b,a
+# print(a, "va", b)
 
 #if10
 # a = int(input(" a = "))
@@ -36,4 +66,92 @@
 #     a=b=0
 # print(a,b)
 
-#
+#if13
+# a = int(input(" a = "))
+# b = int(input(" b = "))
+# c = int(input(" c = "))
+
+# min_number = a
+
+# if b < min_number and c > b:
+#     min_number = b
+# elif c <min_number and b > c:
+#     min_number = c
+
+# max_number = a
+
+# if b > max_number and c < b:
+#     max_number = b
+# elif c > max_number and b < c:
+#     max_number = c 
+# middle = (max_number + min_number) / 2
+# print(middle)
+
+#if14
+# a = int(input(" a = "))
+# b = int(input(" b = "))
+# c = int(input(" c = "))
+
+# min_number = a
+
+# if b < min_number and c > b:
+#     min_number = b
+# elif c <min_number and b > c:
+#     min_number = c
+
+# max_number = a
+
+# if b > max_number and c < b:
+#     max_number = b
+# elif c > max_number and b < c:
+#     max_number = c 
+# if a!=b!=c:
+#     if max_number == a  and min_number == b:
+#         print(min_number,c,max_number)
+#     elif max_number == b and min_number == a:
+#         print(min_number,c,max_number)
+#     elif max_number == a and min_number == c:
+#         print(min_number,b,max_number)
+#     elif max_number == c and min_number == a:
+#         print(min_number,b,max_number)
+#     elif max_number == b and min_number == c:
+#         print(min_number,a,max_number)
+#     elif max_number == c and min_number == b:
+#         print(min_number,a,max_number)
+
+#if15 
+# a = int(input(" a = "))
+# b = int(input(" b = "))
+# c = int(input(" c = "))
+
+# min_number = a
+
+# if b < min_number and c > b:
+#     min_number = b
+# elif c <min_number and b > c:
+#     min_number = c
+
+# max_number = a
+
+# if b > max_number and c < b:
+#     max_number = b
+# elif c > max_number and b < c:
+#     max_number = c 
+# if a!=b!=c:
+#     if max_number == a  and min_number == b:
+#         print(c,max_number)
+#     elif max_number == b and min_number == a:
+#         print(c,max_number)
+#     elif max_number == a and min_number == c:
+#         print(b,max_number)
+#     elif max_number == c and min_number == a:
+#         print(b,max_number)
+#     elif max_number == b and min_number == c:
+#         print(a,max_number)
+#     elif max_number == c and min_number == b:
+#         print(a,max_number)    
+
+
+
+
+
