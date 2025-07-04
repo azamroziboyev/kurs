07d69aa1a40a3e0732ebe523b51ccb36d70e4d00
Azamjon Roziboyev <aroziboyev07@gmail.com>
2025-07-04 08:23:21 +0500

    .

--- if_else.py
+++ if_else.py
@@ -23,3 +23,17 @@
 # else:
 #     a=b=0
 # print(a,b)
+
+#if11
+# a = int(input(" a = "))
+# b = int(input(" b = "))
+# if a!=b:
+#     if a>b:
+#         b=a
+#     else:
+#         a=b
+# else:
+#     a=b=0
+# print(a,b)
+
+#
