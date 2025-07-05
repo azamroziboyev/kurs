e746fe2943a752168bf6cbb9f4a13f004c751b92
Azamjon Roziboyev <aroziboyev07@gmail.com>
2025-07-05 17:29:41 +0500

    .

--- if_else.py
+++ if_else.py
@@ -197,7 +197,77 @@
 # if distance_ac > distance_ab:
 #     print("A nuqtaga eng yaqin nuqta B", f"orasidagi masofa esa {distance_ab}" )
 
-#if2
+#if21
+# x = int(input("x = "))
+# y = int(input("y = "))
+# if x==y==0:
+#     print(0)
+# elif x!=0 and y==0:
+#     print(1)
+# elif x==0 and y!=0:
+#     print(2)
+# else:
+#     print(3)
+
+#if22
+# x = int(input("x = "))
+# y = int(input("y = "))
+# if x>0 and y>0:
+#     print("I chorak")
+# elif x<0 and y>0:
+#     print("II chorak")
+# elif x<0 and y<0:
+#     print("III chorak")
+# else:
+#     print("IV chorak")
+
+#if23
+"""shu misoldi ko'rish kerak"""
+
+#if24
+# x = int(input("x = "))
+# import math
+# if x>0:
+#     f = 2 * math.sin(x)
+# elif x<=0:
+#     f = x - 6
+# print(f)
+
+#if26
+# x = int(input("x = "))
+# if x<=0:
+#     f = -x
+# elif 0<x<2:
+#     f = x**2
+# elif x>=2:
+#     f = 4
+# print(f)
+
+#if27
+# x = int(input("x = "))
+
+
+#if28
+# year = int(input("yil = "))
+# if year < 100:
+#     kabisa_yillar_miqdori = year / 4
+# total_days = (year * 365) + (kabisa_yillar_miqdori * 1)
+# print(total_days)
+
+#if29
+# a = int(input("a = "))
+# if a%2==0 and a >0:
+#     print("musbat juft son")
+# elif a%2!=0 and a>0:
+#     print("musbat toq son")
+# elif a%2==0 and a<0:
+#     print("manfiy juft son")
+# elif a%2!=0 and a<0:
+#     print('manfiy toq son')
+# else:
+#     print("son nolga teng")
+
+#if30
 
 
      
