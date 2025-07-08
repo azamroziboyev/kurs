d195d6bfb69b8d2ce80d4609ab28335deba4aa54
Azamjon Roziboyev <aroziboyev07@gmail.com>
2025-07-08 08:09:11 +0500

    .

--- 19_While_loop.py
+++ 19_While_loop.py
@@ -1,8 +1,10 @@
 
 #While1
-a = int(input("a = "))
-b = int(input("b = "))
-sum = 0
-while sum < a:
-    sum -= b
-print(sum, a - sum)
+# a = int(input("a = "))
+# b = int(input("b = "))
+# sum = 0
+# while sum < a:
+#     sum += b
+# print(sum)
+# print(f"here is the difference:{sum - a}")
+
