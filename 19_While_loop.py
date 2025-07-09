10ca0c02e110d7e707d0b0a3c374557c9a35b25f
Azamjon Roziboyev <aroziboyev07@gmail.com>
2025-07-09 07:09:15 +0500

    .

--- 19_While_loop.py
+++ 19_While_loop.py
@@ -8,3 +8,18 @@
 # print(sum)
 # print(f"here is the difference:{sum - a}")
 
+
+#while2
+# a = int(input("a = "))
+# b = int(input("b = "))
+# sum = 0
+# count = 0
+# while sum < a:
+#     sum += b
+#     count += 1
+#     if (sum + b)>a:
+#         break
+# print(f"A kesmada B kesmani {count} marta joylashtirish mumkin")
+
+#while3
+
