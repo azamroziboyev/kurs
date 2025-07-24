24c31af4fc9de72ca03cd551fd1956b1d68cb8a5
azam <aroziboyev07@gmail.com>
2025-07-24 17:39:48 +0500

    .

--- lesson_42.py
+++ lesson_42.py
@@ -1,15 +1,26 @@
-from datetime import datetime
-def calc_old(name, old):
-    """this function calculates the birth year"""
-    year = datetime.now().year
-    birth_year = year - old
-    print(f"{name} {birth_year}-yilda tug'ilgan")
+# from datetime import datetime
+# def calc_old(name, old):
+#     """this function calculates the birth year"""
+#     year = datetime.now().year
+#     birth_year = year - old
+#     print(f"{name} {birth_year}-yilda tug'ilgan")
+#
+# ism = input("what is your name: ")
+# yosh = int(input("How old are you: "))
+#
+# calc_old(ism, yosh)
 
-ism = input("what is your name: ")
-yosh = int(input("How old are you: "))
 
-calc_old(ism, yosh)
 
+
+# def make_power(raqam):
+#     raqam_kubi = raqam ** 3
+#     raqam_kvadrat = raqam ** 2
+#     return raqam_kvadrat, raqam_kubi
+#
+# number = int(input("butun son kiriting: "))
+# print(f"raqm kubi: {make_power(number)[1]}\n"
+#       f"raqam kvadrati: {make_power(number)[0]}")
 
 
 
