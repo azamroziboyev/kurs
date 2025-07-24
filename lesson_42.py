2ad075a9c7eada29564fb219d33ce23adb2fd4e7
azam <aroziboyev07@gmail.com>
2025-07-24 18:20:23 +0500

    .

--- lesson_42.py
+++ lesson_42.py
@@ -23,4 +23,43 @@
 #       f"raqam kvadrati: {make_power(number)[0]}")
 
 
+"""amlaiyot 3-inchi"""
+# def get_type(number):
+#     if number%2==0:
+#         return "raqam juft"
+#     else:
+#         return "raqam toq"
+#
+# raqam = int(input("Raqamni kiriting men sizga uning toq yoki juftligini aytaman: "))
+# print(get_type(raqam))
+
+"""5-masla"""
+# def make_power(x, y):
+#     return x**y
+#
+# x = int(input("x: "))
+# y = int(input("y: "))
+# print(make_power(x,y))
+
+"""6-masala"""
+# def make_power(x, y=2):
+#     return int(x) ** int(y)
+#
+# x = input("x: ")
+# y = input("y (press Enter to use default 2): ")
+#
+# if y == "":
+#     print(make_power(x))
+# else:
+#     print(make_power(x, y))
+
+# """7-masala"""
+# def bolish_alomatlari(num):
+#     for i in range(1,11):
+#         if num%i==0:
+#             print( f"{num} soni {i} ga qoldiqsiz bo'linadi")
+# raqam = int(input("Bitta son kiriting 10 dan katta: "))
+# bolish_alomatlari(raqam)
+
+
 
